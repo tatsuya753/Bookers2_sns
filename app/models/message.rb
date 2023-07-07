f5e25150747f6f937173da8dbcdfb7066904cfee
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-07 07:54:10 +0000

    fix dm機能

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   
-  belongs_to :user
-  belongs_to :room
 
   validates :message, presence: true, length: { maximum: 140 }
 end
